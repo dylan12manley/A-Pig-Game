4d32e00337eed6d99e76f9259eb649a9c9f1c6ed
Student <example@example.com>
2019-11-06 15:19:44 -0800

    change functionality to js

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,28 @@ function Game(player1, player2) {
   this.player2 = player2
 }
 
-var die = 0;
-var turnScore = 0;
+function Player(roll, turnScore, totalScore) {
+  this.roll = roll,
+  this.turnScore = turnScore,
+  this.totalScore = totalScore
+}
+
+var thisRoll = 0;
+var thisTurnScore = 0;
 var totalScore = 0;
 
-function dieRoll() {
-die = Math.floor(Math.random() * 6) + 1;
+
+
+Player.prototype.diceRoll = function() {
+    this.roll = Math.floor(Math.random() * 6) + 1;
+    if (this.roll === 1) {
+      this.turnScore = 0;
+    } else {
+      this.turnScore += this.roll;
+  }
+  return ((thisRoll = this.roll), (thisTurnScore = this.turnScore));
 }
 
-function addTurnScore() {
-  if (die === 1) {
-    turnScore = 0;
-  } else {
-    turnScore = turnScore + die;
-  }
-}
 
 function addTotalScore() {
   totalScore = totalScore + turnScore;
@@ -25,22 +32,26 @@ function addTotalScore() {
   // $("#player1Btns").toggle();
   // return totalScore;
 }
-
+var player = new Player(0, 0, 0);
 
 $(document).ready(function() {
+  $('#start').click(function(event) {
+    event.preventDefault();
+    var player1Name = $('input#player1Input').val();
+    var player2Name = $('input#player2Input').val();
+    var game = new Game(player1Name, player2Name);
 
-  $('#rollBtn').click(function(event) {
-    event.preventDefault();
-    dieRoll();
-    $('#player1Roll').text(die);
-    addTurnScore();
-    $('#player1TurnScore').text(turnScore);
-    console.log(die);
+
   })
+  $('#rollBtn').click(function(event) {
+    var roll = 0;
+    player.diceRoll(roll);
+    $('#player1Roll').text(thisRoll);
+    $('#player1TurnScore').text(thisTurnScore);
+  })
+  console.log(player.roll);
   $('#holdBtn').click(function(event) {
-    event.preventDefault();
     addTotalScore();
     $('#player1TotalScore').text(totalScore);
-    console.log(totalScore);
   })
 })
